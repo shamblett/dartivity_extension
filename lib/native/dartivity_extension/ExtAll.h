5803d46cfc16fc0017cb56a79dfaa6cedcf65a02
steve <steve.hamblett@linux.com>
2015-09-07 08:35:49 +0100

    Start the core extension structure, iotivity headers now in

--- lib/native/dartivity_extension/ExtAll.h
+++ lib/native/dartivity_extension/ExtAll.h
@@ -14,9 +14,17 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <cstdlib>
+#include <pthread.h>
+#include <condition_variable>
 #include <ostream>
 
 /* Iotivity includes */
+#include "OCPlatform.h"
+#include "OCApi.h"
+
+using namespace OC;
+
 
 /* Dart includes */
 #include "include/dart_native_api.h"
@@ -25,8 +33,8 @@
  * General library wide native functions
  */
 Dart_NativeFunction ResolveName(Dart_Handle name,
-                                int argc,
-                                bool* auto_setup_scope);
+        int argc,
+        bool* auto_setup_scope);
 
 /**
  * General library wide Dart handle checker
