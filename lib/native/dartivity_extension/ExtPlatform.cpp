7a09600e89dd832b234ed658b4fdc31685fe3a5c
steve <steve.hamblett@linux.com>
2015-09-10 10:30:55 +0100

    Debug and formatting

--- lib/native/dartivity_extension/ExtPlatform.cpp
+++ lib/native/dartivity_extension/ExtPlatform.cpp
@@ -20,22 +20,22 @@ void wrappedPlatformCfg(Dart_Port dest_port_id,
     // Get the reply port, we have to assume this is OK or
     // we acn't reply with null to the caller.
     Dart_CObject* param8 = message->value.as_array.values[8];
-    Dart_Port reply_port_id  = param8->value.as_send_port.id;
-    
+    Dart_Port reply_port_id = param8->value.as_send_port.id;
+
     // Arg check and parameter extraction
     if (message->type == Dart_CObject_kArray &&
             9 == message->value.as_array.length) {
-       
+
         Dart_CObject* param0 = message->value.as_array.values[0];
         Dart_CObject* param1 = message->value.as_array.values[1];
         Dart_CObject* param2 = message->value.as_array.values[2];
         Dart_CObject* param5 = message->value.as_array.values[5];
         Dart_CObject* param6 = message->value.as_array.values[6];
         Dart_CObject* param7 = message->value.as_array.values[7];
-       
+
         // Parameter check
         if (param0->type == Dart_CObject_kInt32 &&
-               param1->type == Dart_CObject_kInt32 &&
+                param1->type == Dart_CObject_kInt32 &&
                 param2->type == Dart_CObject_kInt32 &&
                 param5->type == Dart_CObject_kString &&
                 param6->type == Dart_CObject_kInt32 &&
@@ -46,10 +46,19 @@ void wrappedPlatformCfg(Dart_Port dest_port_id,
             std::string ip = std::string(param5->value.as_string);
             int port = param6->value.as_int32;
             dbFile = std::string(param7->value.as_string);
-           
+
             // Setup and call the Iotivity function
             OCPersistentStorage ps{client_open, fread, fwrite, fclose, unlink};
-
+#ifdef DEBUG
+            std::cout << "<<< wrappedPlatformCfg - Param Block >>>" << std::endl;
+            std::cout << "Service Type - " << service << std::endl;
+            std::cout << "Mode - " << mode << std::endl;
+            std::cout << "IP - " << ip << std::endl;
+            std::cout << "Port - " << port << std::endl;
+            std::cout << "Qos - " << qos << std::endl;
+            std::cout << "Db File - " << dbFile << std::endl;
+            std::cout << "<<< Param Block >>>" << std::endl;
+#endif            
             PlatformConfig cfg{
                 (OC::ServiceType)service,
                 (OC::ModeType)mode,
@@ -59,7 +68,7 @@ void wrappedPlatformCfg(Dart_Port dest_port_id,
                 &ps
             };
             OCPlatform::Configure(cfg);
-            
+
             // Build and return the result
             Dart_CObject result;
             result.type = Dart_CObject_kBool;
